658c76ec7ce60c1a5ab6e03e2bcd0ba9b8f95828
Rodrigo Olivarez <rodrigovolivarez@gmail.com>
2025-08-07 15:23:13 -0300

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
-import Router from './router/Router.tsx';
+import Router from './router/Router';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
